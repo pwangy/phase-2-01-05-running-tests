8c03014eb949693c701db2db8b602c5ce53f8002
pwangy <blipsandclicks@gmail.com>
2024-02-17 13:25:08 -0800

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react'
 import Article from './Article'
 import Header from './Header'
 
 function App() {
-  return <div className="App">
-    learn react!
-    <Header/>
-    <Article />
-  </div>;
+	return (
+		<div className='App'>
+			learn react!
+			<Header />
+			<Article />
+		</div>
+	)
 }
 
-export default App;
+export default App
